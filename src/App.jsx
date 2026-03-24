0caa697b7576db99ca8c05ebbaf6d19b2b482bf9
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-03-24 15:54:54 +0100

    first real commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AppLayout from "./layouts/AppLayout"
+import Homepage from "./pages/Homepage"
+import MoviesPage from "./pages/MoviesPage"
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/books" element={<MoviesPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
