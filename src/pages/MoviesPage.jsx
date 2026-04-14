e14699ce25b16bc2fa6f60a88af2e1c2029271a0
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-04-14 16:50:11 +0200

    feat: search filter on

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -6,22 +6,36 @@ import MovieCard from "../components/MovieCard"
 
 export default function MoviesPage() {
     const [movies, setMovies] = useState([])
+    const [search, setSearch] = useState("")
+
+
     useEffect(() => {
         axios.get("http://localhost:3000/api/movies").then(resp => {
             setMovies(resp.data)
         })
     }, [])
+    
+    function handleSearch(event) {
+        event.preventDefault()
+        axios.get("http://localhost:3000/api/movies", { params: { search } }).then(resp => {
+            setMovies(resp.data)
+        })
+    }
 
 
     return (
         <>
-            <div className="container mt-5">
+            <form className="container d-flex justify-content-end mt-5" onSubmit={handleSearch}>
+                <input value={search} onChange={(event) => setSearch(event.target.value)} className="form-control w-25 mx-2" type="search" placeholder="Nome del film"></input>
+                <button type="submit" className="btn btn-primary">Cerca</button>
+            </form>
+            <div className="container mt-3">
                 <h1 className="mb-5">Lista di film</h1>
                 <div className="row row-cols-3 g-5">
                     {
                         movies.map(movie => (
                             <div key={movie.id} className="column">
-                                <MovieCard movie={movie}/>
+                                <MovieCard movie={movie} />
                             </div>
                         ))
                     }
